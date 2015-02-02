b20d4fc327f7e5da9810264436a24dc0eea97ecb
Ben Lubar <ben.lubar@gmail.com>
2015-02-02 10:54:49 -0600

    refactor a bit

--- worker.js
+++ worker.js
@@ -3,68 +3,94 @@
 importScripts('zlib.min.js');
 
 onmessage = function(e) {
-	loadCMV(e.data.file);
+	switch (e.data.mode) {
+	case 'start':
+		startCMV(e.data.file);
+		break;
+	case 'stop':
+		stopCMV(e.data.file);
+		break;
+	default:
+		throw 'unknown mode ' + e.data.mode;
+	}
 };
 
-function loadCMV(path) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', path, true);
-	xhr.overrideMimeType('text\/plain; charset=x-user-defined');
+var movies = {};
 
-	var version = null;
-	var width = null;
-	var height = null;
-	var index = null;
-	var toParse = [];
-	var parseIndex = 0;
-	xhr.onprogress = function(e) {
-		if (version === null && e.loaded >= 4) {
-			version = uint32(xhr.responseText, 0);
-			//console.log(path + ' version: ' + version);
-			if (version < 10000 || version > 10001) {
-				throw path + ' unsupported cmv version ' + version;
-			}
-		}
-		if (width === null && e.loaded >= 4 + 4) {
-			width = uint32(xhr.responseText, 4);
-			//console.log(path + ' width: ' + width);
-		}
-		if (height === null && e.loaded >= 4 + 4 + 4) {
-			height = uint32(xhr.responseText, 4 + 4);
-			//console.log(path + ' height: ' + height);
-		}
-		if (index === null && e.loaded >= 4 + 4 + 4 + 4 + 4) {
-			if (version >= 10001) {
-				// skip sound information for now.
-				var i = 4 + 4 + 4 + 4 + 4 + uint32(xhr.responseText, 4 + 4 + 4 + 4) * 50 + 200 * 16 * 4;
-				if (e.loaded >= i) {
-					index = i;
-					//console.log(path + ' finished header');
-				}
-			} else {
-				index = 4 + 4 + 4 + 4;
-				//console.log(path + ' finished header');
-			}
-		}
-		while (index !== null && e.loaded >= index + 4) {
-			var length = uint32(xhr.responseText, index);
-			if (e.loaded >= index + 4 + length) {
-				var compressed = new Uint8Array(length);
-				for (var i = 0; i < length; i++) {
-					compressed[i] = xhr.responseText.charCodeAt(index + 4 + i) & 0xFF;
-				}
-				var data = new Zlib.Inflate(compressed).decompress();
-				index += 4 + length;
-				toParse.push(data);
-				//console.log(path + ' decompressed: ' + length + ' -> ' + data.length);
-
-				parseIndex = extractFrames(path, toParse, parseIndex, version, width, height);
-			} else {
-				break;
-			}
-		}
+function startCMV(path) {
+	var movie = {
+		xhr:        new XMLHttpRequest(),
+		path:       path,
+		version:    null,
+		width:      null,
+		height:     null,
+		index:      null,
+		toParse:    [],
+		parseIndex: 0
 	};
-	xhr.send(null);
+	movie.xhr.open('GET', path, true);
+	movie.xhr.overrideMimeType('text/plain; charset=x-user-defined');
+	movie.xhr.onprogress = cmvProgress.bind(movie);
+	movie.xhr.send(null);
+
+	movies[path] = movie;
+}
+
+function stopCMV(path) {
+	if (!(path in movies)) {
+		console.log(path + ' already stopped');
+		return;
+	}
+	movies[path].xhr.abort();
+	delete movies[path];
+}
+
+function cmvProgress(e) {
+	if (this.version === null && e.loaded >= 4 * 1) {
+		this.version = uint32(this.xhr.responseText, 4 * 0);
+		console.log(this.path + ' version: ' + this.version);
+		if (this.version < 10000 || this.version > 10001) {
+			throw this.path + ' unsupported cmv version ' + this.version;
+		}
+	}
+	if (this.width === null && e.loaded >= 4 * 2) {
+		this.width = uint32(this.xhr.responseText, 4 * 1);
+		console.log(this.path + ' width: ' + this.width);
+	}
+	if (this.height === null && e.loaded >= 4 * 3) {
+		this.height = uint32(this.xhr.responseText, 4 * 2);
+		console.log(this.path + ' height: ' + this.height);
+	}
+	if (this.index === null && e.loaded >= 4 * 5) {
+		if (this.version >= 10001) {
+			// skip sound information for now.
+			var i = 4 * 5 + uint32(this.xhr.responseText, 4 * 4) * 50 + 200 * 16 * 4;
+			if (e.loaded >= i) {
+				this.index = i;
+				console.log(this.path + ' finished header');
+			}
+		} else {
+			this.index = 4 * 4;
+			console.log(this.path + ' finished header');
+		}
+	}
+	while (this.index !== null && e.loaded >= this.index + 4) {
+		var length = uint32(this.xhr.responseText, this.index);
+		if (e.loaded >= this.index + 4 + length) {
+			var compressed = new Uint8Array(length);
+			for (var i = 0; i < length; i++) {
+				compressed[i] = this.xhr.responseText.charCodeAt(this.index + 4 + i) & 0xFF;
+			}
+			var data = new Zlib.Inflate(compressed).decompress();
+			this.index += 4 + length;
+			this.toParse.push(data);
+			console.log(this.path + ' decompressed: ' + length + ' -> ' + data.length);
+
+			this.parseIndex = extractFrames(this.path, this.toParse, this.parseIndex, this.version, this.width, this.height);
+		} else {
+			break;
+		}
+	}
 }
 
 function extractFrames(path, toParse, index, version, width, height) {
